e86c467e8845fee9fb70fb8cf621bfe51b5c4bc1
ngot <zhuanghengfei@gmail.com>
2017-07-10 23:59:39 +0800

    release

--- assets/js/your-custom.js
+++ assets/js/your-custom.js
@@ -1,1 +1,313 @@
 /* Your Custom JS below */
+(function() {
+  var config = [
+    { 
+      src: '/wedding/b1.JPG',
+      cut: 'small',
+      category: 'beach',
+    },
+    {
+      src: '/wedding/b2.JPG',
+      cut: 'small',
+      category: 'beach',
+    },
+    { 
+      src: '/wedding/b3.JPG',
+      cut: 'small',
+      category: 'beach',
+    },
+    { 
+      src: '/wedding/b4.JPG',
+      cut: 'small',
+      category: 'beach',
+    },
+    { 
+      src: '/wedding/t1.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t2.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t3.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t4.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t5.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t6.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t7.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t8.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t9.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t10.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t11.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t12.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t13.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t14.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t15.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t16.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t17.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t18.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t19.JPG',
+      cut: 'small/rotate/90',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t20.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t21.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t22.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t23.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t24.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t25.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t26.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t27.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t28.JPG',
+      cut: 'small/rotate/180',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t29.JPG',
+      cut: 'small/rotate/180',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t30.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t31.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t32.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t33.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t34.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t35.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t36.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t37.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t38.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t39.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t40.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t41.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t42.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t43.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t44.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t45.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t46.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t47.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t48.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t49.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t50.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t51.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t52.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+    { 
+      src: '/wedding/t53.JPG',
+      cut: 'small',
+      category: 'travel',
+    },
+  ];
+
+  var prefix = 'http://wileam-images.b0.upaiyun.com';
+
+  var tpl = `
+<li class="img-treatment {{category}}">
+    <a class="pop-gallery-img popup-indicator" href="{{prefix}}{{src}}!{{cut}}">
+    <img src="{{prefix}}{{src}}!{{cut}}" alt="add a description to get better accessibility" />
+    </a> 
+</li>  
+`;
+
+  var photos = config.map(el => {
+    return render(tpl, Object.assign({}, el, { prefix }));
+  });
+
+  function render(str, data) {
+    return str.replace(/\{\{\s*([\$_a-zA-Z]+[0-9]*)\s*\}\}/ig, (a, b) => data[b] || '');
+  }
+
+  window.onload = function() {
+    var el = $('.feature-section .container .pop-gallery');
+    el.append(photos);
+  };
+})();
